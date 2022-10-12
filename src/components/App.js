af28241a5748485076e40b60e74187eb4f983558
gacdutra <ga.acdutra@gmail.com>
2022-10-12 09:36:53 -0300

    feat: add UserContext

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import GlobalStyle from "../styles/globalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup";
 import Signin from "./Pages/Signin";
+import { UserStorage } from "../contexts/UserContext";
 
 export default function App() {
   return (
     <>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/sign-up" element={<Signup />} />
-          <Route path="/sign-in" element={<Signin />} />
-        </Routes>
-      </BrowserRouter>
+      <UserStorage>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/sign-up" element={<Signup />} />
+            <Route path="/sign-in" element={<Signin />} />
+          </Routes>
+        </BrowserRouter>
+      </UserStorage>
     </>
   );
 }
